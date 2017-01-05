53a49188a8724741445ec627b4a2c5249c732842
flajole <dmizubkov@gmail.com>
2017-01-05 16:10:53 +0100

    Rename files in source

--- R/00 - sources_utility.R
+++ R/00 - sources_utility.R
@@ -10,10 +10,10 @@ source("R/01 - tidy_PQI.R")
 source("R/02 - make_annID.R")
 source("R/03 - filter_unique_peptides.R")
 source("R/04 - norm_transform_fillNA.R")
-source("R/report_PQI.R")
-source("R/report_quality.R")
-source("R/report_abundance_distribution.R")
-source("R/report_plot_CV_hist.R")
+source("R/99 - report_PQI.R")
+source("R/99 - report_quality.R")
+source("R/99 - report_abundance_distribution.R")
+source("R/99 - report_plot_CV_hist.R")
 
 zero.to.na <- function(x) {
   x[x==0] <- NA
