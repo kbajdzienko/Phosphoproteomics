a40d14fc6f6badd615f873980b762cb3404bc162
flajole <dmizubkov@gmail.com>
2017-01-09 17:42:33 +0100

    Add pep_exp_mz and pep_delta to reading

--- R/01.2 - mascot_pep_query.R
+++ R/01.2 - mascot_pep_query.R
@@ -33,11 +33,12 @@ read.mascot <- function(mascot_file, data = "pep") {
       mascot %>%
       select(prot_acc, prot_score,
              pep_query, pep_isbold, pep_isunique,
-             pep_start, pep_end, pep_miss, pep_exp_mr,
+             pep_start, pep_end, pep_miss,
+             pep_exp_mz, pep_exp_mr, pep_delta,
              pep_score, pep_seq, pep_var_mod, pep_var_mod_pos,
              pep_scan_title) %>%
       mutate_at(vars(prot_score:pep_miss), as.integer) %>%
-      mutate_at(vars(pep_exp_mr, pep_score), as.numeric) %>%
+      mutate_at(vars(pep_exp_mz:pep_score), as.numeric) %>%
       mutate_at(vars(pep_isbold, pep_isunique), as.logical) %>%
       rename(query_number = pep_query)
 
