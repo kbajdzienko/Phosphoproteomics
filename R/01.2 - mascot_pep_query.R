08cf53b286968a396780a3e697474dd72ed76e3f
flajole <dmizubkov@gmail.com>
2017-01-09 14:53:46 +0100

    ADD: mascot pep and query filtering after reading FIX: the case with no query data

--- R/01.2 - mascot_pep_query.R
+++ R/01.2 - mascot_pep_query.R
@@ -1,13 +1,24 @@
 # Functions reading mascot files
 
-read.mascot <- function(mascot_file, data = c("pep", "query")) {
+read.mascot <- function(mascot_file, data = "pep") {
+  match.arg(data, c("pep", "query"))
   mascot <- readLines(mascot_file)
+
   if (data == "pep") {
+    # Read peptides table till queries table or till the end if there is no query data
     skip <- grep("prot_hit_num", mascot) - 1
-    nrows <- grep("Queries", mascot) - grep("prot_hit_num", mascot) - 1
+    if (any(grepl("Queries", mascot))) {
+      nrows <- grep("Queries", mascot) - grep("prot_hit_num", mascot) - 1
+    } else {
+      nrows <- -1L
+    }
   } else if (data == "query") {
+    # Read query table till the end
     skip <- grep("query_number", mascot) - 1
-    nrows = -1L
+    if (length(skip) == 0) {
+      stop("No query data detected in mascot file")
+    }
+    nrows <- -1L
   }
 
   mascot <- tbl_df(read.csv(mascot_file,
@@ -16,7 +27,29 @@ read.mascot <- function(mascot_file, data = c("pep", "query")) {
                             header = T, sep = ",",
                             stringsAsFactors = F,
                             colClasses = "character"))
+
+  if (data == "pep") {
+    mascot <-
+      mascot %>%
+      select(prot_acc, prot_score,
+             pep_query, pep_isbold, pep_isunique,
+             pep_start, pep_end, pep_miss,
+             pep_score, pep_seq, pep_var_mod, pep_var_mod_pos,
+             pep_scan_title) %>%
+      mutate_at(vars(prot_score:pep_miss), as.integer) %>%
+      mutate_at(vars(pep_score), as.numeric) %>%
+      mutate_at(vars(pep_isbold, pep_isunique), as.logical) %>%
+      rename(query_number = pep_query)
+
+  } else if (data == "query") {
+    mascot <-
+      mascot %>%
+      filter(pep_seq != "") %>%
+      select(query_number, pep_score, pep_seq,
+             pep_var_mod, pep_var_mod_pos, pep_scan_title) %>%
+      mutate_at(vars(query_number), as.integer) %>%
+      mutate_at(vars(pep_score), as.numeric)
+  }
+
   return(mascot)
 }
-
-
