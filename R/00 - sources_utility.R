f388988d19ad9ff60560b7d8f8c499e5e2be7fe9
flajole <dmizubkov@gmail.com>
2017-01-09 19:49:18 +0100

    UPD sources, FIX the plyr bug

--- R/00 - sources_utility.R
+++ R/00 - sources_utility.R
@@ -1,10 +1,13 @@
+if (!require("plyr", character.only = T)) install.packages("plyr")
 if (!require("tidyverse", character.only = T)) install.packages("tidyverse")
 
+library(plyr)
 library(tidyverse)
 
 source("R/01.1 - tidy_PQI.R")
 source("R/01.2 - mascot_pep_query.R")
 source("R/01.3 - intData_formats.R")
+source("R/01.4 - read_ions.R")
 source("R/02 - make_annID.R")
 source("R/03 - filter_unique_peptides.R")
 source("R/04 - norm_transform_fillNA.R")
@@ -13,8 +16,9 @@ source("R/06 - phos_sites_merging.R")
 source("R/99 - report_PQI.R")
 #source("R/99 - report_quality.R")
 source("R/99 - report_quality_DF.R")
-source("R/99 - report_abundance_distribution.R")
 source("R/99 - report_plot_CV_hist.R")
+source("R/99 - report_abundance_distribution.R")
+source("R/99 - report_protein_profile.R")
 
 zero.to.na <- function(x) {
   x[x==0] <- NA
