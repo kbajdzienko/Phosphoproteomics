53e9eb4a890bd603882aef47a80701f70566dda7
flajole <dmizubkov@gmail.com>
2017-01-11 23:13:18 +0100

    ADD: curation of NA produced by isoleucin/leucin bug

--- R/02 - make_annID.R
+++ R/02 - make_annID.R
@@ -1,41 +1,26 @@
-# Creata ann_ID in format "AT3G47070.1_T65_T66"
+# Create ann_ID in format "AT3G47070.1_T65_T66"
 # Accession, phosphorilated amino acid, number
 
 make_annID <- function(df, mascot_file) {
 
-  # Reads mascot file, takes peptide table and joins pep_var_mod_conf from query table
-  mascot_conf <- function(mascot_file) {
-    dupl <- function(x) duplicated(x) | duplicated(x, fromLast = T)
-
-    query <- read.mascot(mascot_file, "query")
-    mascot <- read.mascot(mascot_file, "pep")
-    query$pep_var_mod_conf[!dupl(query$query_number)] <- 100
-    mascot <- left_join(mascot, query)
-    return(mascot)
-  }
-
-  # Remove everything but Sequence, Accession, start and end position
-  # and number of missed cleaveges from mascot output file
+  # Read mascot and rename some variables for future joining
   mascot <-
-    mascot_conf(mascot_file) %>%
-    select(prot_acc, pep_seq, pep_score,
-           pep_start, pep_end, pep_miss,
-           pep_exp_mr, pep_var_mod_conf) %>%
+    read.mascot(mascot_file) %>%
     rename(Accession = prot_acc,
            Sequence = pep_seq,
            Score = pep_score,
            Neutral_mass = pep_exp_mr) %>%
     distinct()
 
-  # Round Neutral_mass to get match between df and mascot
-  mascot <- mutate(mascot, Neutral_mass = round(Neutral_mass, 4))
-  df$peakData <- mutate(df$peakData, Neutral_mass = round(Neutral_mass, 4))
+  # Function removing NAs by considering I/L as the same
+  mascot <- cure_IL_NA(df, mascot)
 
-  # Merge original annotation data table with mascot
+  # Remove everything but Sequence, Accession, start and end position
+  # and number of missed cleaveges and
+  # merge original annotation data table with mascot
   peakData <-
     df$peakData %>%
-    left_join(mascot)
-
+    left_join(select(mascot, Accession, Sequence, pep_miss, pep_start, pep_end))
 
   # Separate modification columns into single modifications
   # Select only phospho
@@ -65,4 +50,53 @@ make_annID <- function(df, mascot_file) {
     select(peak_ID:Score, ann_ID, everything())
 
   return(df)
+}
+
+
+# Function, adding to mascot missing Sequence/Accessions in case of I/L bug
+cure_IL_NA <- function (df, mascot){
+  # Funciton fixing NAs appearance
+  IL_seq_na <-
+    df$peakData %>%
+    anti_join(mascot, by = c("Sequence", "Accession")) %>%
+    distinct(Sequence, Accession)
+
+  # Form regular expression instead of sequence with optional I/L positions
+  IL_seq_na$ILSequence <-
+    IL_seq_na$Sequence %>%
+    strsplit("") %>%
+    lapply(gsub, pattern = "[LI]", replacement = "[LI]") %>%
+    lapply(function(x) paste0(c("^", x, "$"), collapse = "")) %>%
+    unlist()
+
+  pep_starts <- list()
+  for (i in 1:nrow(IL_seq_na)) {
+    add_data <-
+      mascot %>%
+      filter(Accession == IL_seq_na$Accession[i] & grepl(IL_seq_na$ILSequence[i], Sequence)) %>%
+      distinct(pep_start, pep_end, pep_miss)
+    if (nrow(add_data) > 1) {
+      warning("Multiple I/L matches found in Mascot for ",
+              IL_seq_na$Sequence[i], " ", IL_seq_na$Accession[i])
+      add_data <-
+        data.frame(pep_start = NA, pep_end = NA, pep_miss = NA) %>%
+        mutate_all(as.integer) %>%
+        tbl_df()
+    } else if (nrow(add_data) == 0) {
+      warning("No I/L matches found in Mascot for ",
+              IL_seq_na$Sequence[i], " ", IL_seq_na$Accession[i])
+      add_data <-
+        data.frame(pep_start = NA, pep_end = NA, pep_miss = NA) %>%
+        mutate_all(as.integer) %>%
+        tbl_df()
+    }
+    pep_starts[[i]] <- add_data
+  }
+
+  mascot <-
+    IL_seq_na %>%
+    bind_cols(Reduce(bind_rows, pep_starts)) %>%
+    select(-ILSequence) %>%
+    bind_rows(mascot, .)
+  return(mascot)
 }
