9205b7483eac9ad3a8482037cb93465af98d3e87
kbajdzienko <krzysztof.bajdzienko@gmail.com>
2017-01-16 13:37:34 +0100

    Update source, Rename scripts

--- R/00 - data_processing_pipeline.R
+++ R/00 - data_processing_pipeline.R
@@ -5,30 +5,44 @@
 #           peptide_file = "", 
 #           min_score = 10)
 
-dtt <- function(df, peptide_file, mascot_file, ions_file, 
-                abundance = "Raw", NA_allowed = 0.5, 
-                min_score = 10, NAimputation = "ppca", base = 2) {
+
+
+dtt_pre <- function(df, peptide_file, mascot_file, 
+                    ions_file, 
+                abundance = "Raw", NA_allowed = 0.5, base = 2) {
   
   df <- tidy_PQI(file = peptide_file, abundance = abundance)
   
-  # Creata ann_ID in format "AT3G47070.1_T65_T66"
-  # Accession, phosphorylated amino acid, number
   df <- make_annID(df, mascot_file = mascot_file)
   
-  # Remove peptides with more than one Accession based on ion PQI output table
-  #WARNING: This function can return a warning: NAs introduced by coercion. This is because peptides
-  #after charge deconvolution in QI can be left with no score
   df <- filter_pep_unique(df, ions_file = ions_file)
   
-  # Normalization by median intensity of all the peaks in sample
-  df <- normMedian(df)
-  
-  # Filter peaks with too many NA.
-  # Keep a peak if in at least one group there is less NA, then the set threshold
   df <- filter_NA(df, threshold = NA_allowed)  
   
-  # Filter peaks with Score less than the set threshold
-  df <- filter_score(df, score_threshold = min_score)
+  df <- logTransform(df, base = base) 
+  
+  return(df)
+  
+}
+
+
+
+
+
+dtt <- function(df, peptide_file, mascot_file, 
+                ions_file, 
+                abundance = "Raw", NA_allowed = 0.5, 
+                NAimputation = "ppca", base = 2) {
+  
+  df <- tidy_PQI(file = peptide_file, abundance = abundance)
+  
+  df <- make_annID(df, mascot_file = mascot_file)
+  
+  df <- filter_pep_unique(df, ions_file = ions_file)
+
+  df <- normMedian(df)
+  
+  df <- filter_NA(df, threshold = NA_allowed)  
   
   # Fill missing values with PCA-based methods (PPCA or BPCA)
   df <- fillNA(df, method = NAimputation)
--- R/00 - sources_utility.R
+++ R/00 - sources_utility.R
@@ -1,10 +1,11 @@
+
 if (!require("plyr", character.only = T)) install.packages("plyr")
 if (!require("tidyverse", character.only = T)) install.packages("tidyverse")
 
 library(plyr)
 library(tidyverse)
 
-
+source("R/00 - data_processing_pipeline.R")
 source("R/01.1 - tidy_PQI.R")
 source("R/01.2 - read_mascot.R")
 source("R/01.3 - intData_formats.R")
@@ -16,12 +17,13 @@ source("R/04 - norm_transform_fillNA.R")
 source("R/05 - filter_NA_filter_score.R")
 source("R/06 - phos_sites_merging.R")
 source("R/99 - report_PQI.R")
-source("R/99 - report_quality.R")
+source("R/99 - report_quality_new.R")
 source("R/99 - report_quality_DF.R")
 source("R/99 - report_plot_CV_hist.R")
 source("R/99 - report_abundance_distribution.R")
 source("R/99 - report_protein_profile.R")
 source("R/99 - report_mass_accuracy.R")
+source("R/99 - report_plot_norm_summary.R")
 
 zero.to.na <- function(x) {
   x[x==0] <- NA
