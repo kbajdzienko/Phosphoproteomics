d075a3b8eada6ea2d2b202ce28cbd5c695913e32
flajole <dmizubkov@gmail.com>
2017-01-17 17:32:11 +0100

    ADD: normMean, multiplication be total mean/median during normalization, test if there are NAs

--- R/04 - norm_transform_fillNA.R
+++ R/04 - norm_transform_fillNA.R
@@ -1,12 +1,25 @@
 # Normalization by median intensity of all the peaks in sample
 normMedian <- function(df) {
+  total.median <- median(df$intData$intensity, na.rm = T)
   df$intData <-
     df$intData %>%
     group_by(sample_ID) %>%
-    mutate(intensity = intensity/median(intensity, na.rm = T)) %>%
+    mutate(intensity = total.median*intensity/median(intensity, na.rm = T)) %>%
     ungroup()
   return(df)
 }
+
+# Normalization by mean intensity of all the peaks in sample
+normMean <- function(df) {
+  total.mean <- mean(df$intData$intensity, na.rm = T)
+  df$intData <-
+    df$intData %>%
+    group_by(sample_ID) %>%
+    mutate(intensity = total.mean*intensity/mean(intensity, na.rm = T)) %>%
+    ungroup()
+  return(df)
+}
+
 
 # Mean-centering and scaling
 normScale <- function(df, method = "auto") {
@@ -26,6 +39,10 @@ normScale <- function(df, method = "auto") {
 # Fill missing values with PCA-based methods (PPCA or BPCA)
 fillNA <- function(df, method = "ppca") {
   match.arg(method, c("ppca", "bpca"))
+  if (!any(is.na(df$intData$intensity))) {
+    warning("No NA detected")
+    return(df)
+  }
   intMatrix <-
     intMatrix(df) %>%
     t() %>%
