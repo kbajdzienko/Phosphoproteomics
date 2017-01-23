25837d89bd5f77226800975169c73262d93ada5b
flajole <dmizubkov@gmail.com>
2017-01-23 19:56:24 +0100

    ADD: filter_NA_Mann according to Mann's article

--- R/05 - filter_NA_filter_score.R
+++ R/05 - filter_NA_filter_score.R
@@ -20,6 +20,28 @@ filter_NA <- function(df, threshold = 0.5) {
   return(df)
 }
 
+
+# Filter peaks with too many NA.
+# Keep a peak if in at least 2 non-NAs in each group or
+# at least 3 non-NAs in 3/4 of groups
+
+filter_NA_Mann <- function(df) {
+  shitpeaks <-
+    df$intData %>%
+    left_join(df$sampleData) %>%
+    mutate(intensity = zero.to.na(intensity)) %>%
+    group_by(peak_ID, group) %>%
+    summarize(non_NA = sum(!is.na(intensity))) %>%
+    group_by(peak_ID) %>%
+    filter(all(non_NA >= 2) | sum(non_NA >= 3)/n() > 0.75) %>%
+    ungroup() %>%
+    select(peak_ID) %>%
+    distinct()
+  df$peakData <- anti_join(df$peakData, shitpeaks)
+  df$intData <- anti_join(df$intData, shitpeaks)
+  return(df)
+}
+
 # Filter peaks with Score less than the set threshold
 
 filter_score <- function(df, score_threshold = 5) {
@@ -38,12 +60,12 @@ filter_score <- function(df, score_threshold = 5) {
 #MD score refers to confidence of phosphorylation site arrangment
 
 filter_phos_conf <- function(df, score_threshold = 5) {
-  
+
   shitpeaks <-
     df$peakData %>%
     filter(pep_var_mod_conf < score_threshold) %>%
     select(peak_ID)
-  
+
   df$peakData <- anti_join(df$peakData, shitpeaks)
   df$intData <- anti_join(df$intData, shitpeaks)
   return(df)
