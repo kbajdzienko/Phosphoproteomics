e215b3e4b21a20b774298a326cb1296e6d401697
flajole <dmizubkov@gmail.com>
2017-01-25 15:52:43 +0100

    ADD: subset df by samples or accessions

---
+++ R/99 - subset_df.R
@@ -0,0 +1,16 @@
+subset_df_samples <- function(df, pattern) {
+  shitsamples <- filter(df$sampleData, !grepl(pattern, sample_ID)) %>% select(sample_ID)
+  df$sampleData <- anti_join(df$sampleData, shitsamples)
+  df$intData <- anti_join(df$intData, shitsamples)
+  df$annIntData <- anti_join(df$annIntData, shitsamples)
+  return(df)
+}
+
+
+subset_df_acc <- function(df, acc_list) {
+  df$annData <- filter(df$annData, Accession %in% acc_list)
+  df$peakData <- filter(df$peakData, Accession %in% acc_list)
+  df$intData <- semi_join(df$peakData, by = Accession)
+  df$annIntData <- semi_join(df$annData, by = Accession)
+  return(df)
+}
