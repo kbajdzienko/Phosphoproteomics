6d3ceff4b7d831695c05347fb43b4057f761e6a2
kbajdzienko <krzysztof.bajdzienko@gmail.com>
2017-02-08 16:16:04 +0100

    fix semi_join parameters

--- R/99 - subset_df.R
+++ R/99 - subset_df.R
@@ -10,7 +10,7 @@ subset_df_samples <- function(df, pattern) {
 subset_df_acc <- function(df, acc_list) {
   df$annData <- filter(df$annData, Accession %in% acc_list)
   df$peakData <- filter(df$peakData, Accession %in% acc_list)
-  df$intData <- semi_join(df$peakData, by = Accession)
-  df$annIntData <- semi_join(df$annData, by = Accession)
+  df$intData <- semi_join(df$peakData,df$intData, by = Accession)
+  df$annIntData <- semi_join(df$annData,df$annIntData, by = Accession)
   return(df)
 }
