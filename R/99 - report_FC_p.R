815f0573e4675406c86d3c505e777289d40feb0c
kbajdzienko <krzysztof.bajdzienko@gmail.com>
2017-02-10 07:12:07 +0100

    Make function to calculate FC and pval for psites or peaks, based on plot_volcano function.

---
+++ R/99 - report_FC_p.R
@@ -0,0 +1,32 @@
+report_FC_p <- function(df, group1, group2,
+                             method = "t.test",
+                            data.type = "peakID",
+                             FC.threshold = 2,
+                             p.threshold = 0.05) {
+  
+  match.arg(method, c("t.test", "wilcox.test"))
+  match.arg(data.type, c("peakID","psite"))
+  
+  if (!all(c(group1, group2) %in% df$sampleData$group))
+    stop("Selected group names are not found")
+  
+  if (data.type=="psite"){
+  stat_mat <-
+    df$sampleData %>%
+    filter(group %in% c(group1, group2)) %>%
+    left_join(df$annIntData) %>%
+    group_by(ann_ID) %>%
+    summarize(p.value = do.call(method, list(intensity[group == group1], intensity[group == group2]))$p.value,
+              FC = mean(intensity[group == group1], na.rm = T)/mean(intensity[group == group2], na.rm = T)) %>%
+    mutate(signif = abs(p.value) < p.threshold & abs(FC) > FC.threshold) %>%
+    filter(signif==TRUE)}  else if(data.type=="peakID"){
+  stat_mat <-  filter(df$sampleData, group %in% c(group1, group2)) %>%
+  left_join(df$intData) %>%
+    group_by(peak_ID) %>%
+    summarize(p.value = do.call(method, list(intensity[group == group1], intensity[group == group2]))$p.value,
+              FC = mean(intensity[group == group1], na.rm = T)/mean(intensity[group == group2], na.rm = T)) %>%
+    mutate(signif = abs(p.value) < p.threshold & abs(FC) > FC.threshold) %>%
+    filter(signif==TRUE)}
+  return(stat_mat)
+}
+  
