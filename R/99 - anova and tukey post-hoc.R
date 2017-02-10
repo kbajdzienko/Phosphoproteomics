b9213456616d228cdb56573f32124f2ed8d0d253
kbajdzienko <krzysztof.bajdzienko@gmail.com>
2017-02-10 07:10:29 +0100

    minor fix and optimization for further stat analysis

--- R/99 - anova and tukey post-hoc.R
+++ R/99 - anova and tukey post-hoc.R
@@ -22,7 +22,10 @@ groups <- factor(arrange(df$sampleData, group)$group,
                  levels = unique(arrange(df$sampleData, group)$group)) 
 p.values <- apply(intMatrix(df),1, anova.setup, groups)
 df_p.values <- p.adjust(p.values, "BH")
-return(as.data.frame(df_p.values))
+df_p.values <- as.data.frame(df_p.values) %>% filter(df_p.values <0.05)
+df_p.values <- cbind(peakID = rownames(df_p.values), df_p.values)
+rownames(df_p.values) <- NULL
+return(df_p.values)
 }
 #Post hoc test for DF
 df_tukey <- function(df){
@@ -39,7 +42,7 @@ gp <- getPairs(groups)
       })
 df.tuk <- t(tuk.p.values)
 df.tuk<- as.data.frame(df.tuk)
-df.tuk <- cbind(peakID = rownames(df1.tuk), df.tuk)
+df.tuk <- cbind(peakID = rownames(df.tuk), df.tuk)
 rownames(df.tuk) <- NULL
 df.tuk <- gather(df.tuk, contains("-"), key="Comparison", value="p.val") %>% 
   filter(p.val<0.05) %>% 
@@ -47,11 +50,3 @@ df.tuk <- gather(df.tuk, contains("-"), key="Comparison", value="p.val") %>%
 
 return(df.tuk)
 }
-
-df18tuk2 <- as.data.frame(df18a_tukey)
-df18tuk2 <- cbind(peakID = rownames(df18tuk2), df18tuk2)
-rownames(df18tuk2) <- NULL
-
-
-df18tuk2 <- gather(df18tuk2,contains("-"), key="Comparison", value="p.val") %>% filter(p.val<0.05) %>% arrange(p.val)
-
