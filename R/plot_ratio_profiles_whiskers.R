41f7b003d19b04e2d4810810452db48553d1748c
flajole <dmizubkov@gmail.com>
2017-02-16 08:03:06 +0300

    plot_protein_profile_ratio with whiskers

---
+++ R/plot_ratio_profiles_whiskers.R
@@ -0,0 +1,58 @@
+# Plot MSstat protein profile
+# Example:
+# plot_acc(df, "AT3G60600.1")
+
+plot_protein_profile_ratio <- function(df,
+                                       protein,
+                                       smooth = TRUE,
+                                       whiskers = TRUE) {
+  require(ggplot2)
+
+  # Log transform if not
+  if (!is.log(df$intData$intensity)) df <- logTransform(df)
+
+  data <-
+    df$annIntData %>%
+    left_join(df$sampleData) %>%
+    group_by(ann_ID, treatment, time) %>%
+    summarise(int_mean = mean(intensity, na.rm = T),
+              int_sd = sd(intensity, na.rm = T)) %>%
+    group_by(ann_ID, time) %>%
+    summarize(ratio = int_mean[treatment == "AZD/GLU"]-int_mean[treatment == "GLU"],
+              int_sd = int_sd[treatment == "AZD/GLU"] + int_sd[treatment == "GLU"]) %>%
+    ungroup() %>%
+    left_join(df$annData) %>%
+    filter(Accession == protein) %>%
+    mutate(ann_ID = gsub("^[[:alnum:]]+.*[[:alnum:]]*_", "", ann_ID)) %>%
+    mutate(Position = as.integer(gsub("[[:alpha:]]+", "", ann_ID))) %>%
+    arrange(Position)
+
+  # Reorder levels of factor ann_ID by position number for plot legend
+  data$ann_ID <- reorder(data$ann_ID, data$Position)
+
+
+  # Plot
+  ggplot(data, aes(x = jitter(time, 2), y = ratio, group = ann_ID, color = ann_ID,
+                   ymin = ratio - int_sd, ymax = ratio + int_sd)) +
+    do.call(paste0("geom_", if_else(smooth, "smooth", "line")), list(size = 0.5)) +
+    geom_errorbar(alpha = 0.5*whiskers, width = 5) +
+    scale_y_continuous('Log2-ratio') +
+    scale_x_continuous('Time, min') +
+    labs(title = protein) +
+    theme(
+      panel.background = element_rect(fill = 'white', colour = "black"),
+      legend.key = element_rect(fill = 'white', colour = 'white'),
+      panel.grid.minor = element_blank(),
+      strip.background = element_rect(fill = 'gray95'),
+      strip.text.x = element_text(colour = c("#00B0F6"), size = 14),
+      axis.text.x = element_text(size = 10, colour="black"),
+      axis.text.y = element_text(size = 10, colour="black"),
+      axis.ticks = element_line(colour = "black"),
+      axis.title.x = element_text(size = 15, vjust = -0.4),
+      axis.title.y = element_text(size = 15, vjust = 0.3),
+      title=element_text(size = 18, vjust = 1.5),
+      #legend.position = "top",
+      legend.text = element_text(size = 7),
+      legend.title = element_blank()
+    )
+}
