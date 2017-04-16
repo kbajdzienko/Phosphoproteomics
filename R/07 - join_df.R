d93a397cf1c31797891962f1d3489aad98a9d51e
kbajdzienko <krzysztof.bajdzienko@gmail.com>
2017-04-16 17:33:04 +0200

    Minor fix

--- R/07 - join_df.R
+++ R/07 - join_df.R
@@ -2,9 +2,10 @@ join_df <- function(df1, df2) {
   if(is.null(df1$annData) | is.null(df2$annData))
     stop("Merge sites first to get annData")
   sampleData <- bind_rows(df1$sampleData, df2$sampleData)
-  annData <- inner_join(df1$annData, df2$annData)
+  annData <- inner_join(df1$annData, df2$annData, by="ann_ID")
   annIntData <- bind_rows(semi_join(df1$annIntData, annData),
                           semi_join(df2$annIntData, annData))
+    
   df <- list(sampleData = sampleData,
              annData = annData,
              annIntData = annIntData)
