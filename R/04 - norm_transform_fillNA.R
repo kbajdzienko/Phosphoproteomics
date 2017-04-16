e42fa55354906d91e87dc1c3e1cbf0e694bdabb7
kbajdzienko <krzysztof.bajdzienko@gmail.com>
2017-04-16 17:32:19 +0200

    Add normalization and scaling functions that work on phosphorylation site data.

--- R/04 - norm_transform_fillNA.R
+++ R/04 - norm_transform_fillNA.R
@@ -8,6 +8,15 @@ normMedian <- function(df) {
     ungroup()
   return(df)
 }
+normMedian_ann <- function(df) {
+  total.median <- median(df$annIntData$intensity, na.rm = T)
+  df$annIntData <-
+    df$annIntData %>%
+    group_by(sample_ID) %>%
+    mutate(intensity = total.median*intensity/median(intensity, na.rm = T)) %>%
+    ungroup()
+  return(df)
+}
 
 # Normalization by mean intensity of all the peaks in sample
 normMean <- function(df) {
@@ -30,6 +39,18 @@ normScale <- function(df, method = "auto") {
   df$intData <-
     df$intData %>%
     group_by(peak_ID) %>%
+    mutate_at(vars(intensity), method) %>%
+    ungroup()
+  return(df)
+}
+normScale_ann <- function(df, method = "auto") {
+  match.arg(method, c("auto", "pareto"))
+  auto <- function(x) (x - mean(x, na.rm = T))/sd(x, na.rm = T)
+  pareto <- function(x) (x - mean(x, na.rm = T))/sqrt(sd(x, na.rm = T))
+  
+  df$annIntData <-
+    df$annIntData %>%
+    group_by(ann_ID) %>%
     mutate_at(vars(intensity), method) %>%
     ungroup()
   return(df)
@@ -70,6 +91,20 @@ logTransform <- function(df, base = 2) {
   }
   df$intData <- mutate(df$intData, intensity = log(intensity, base = base))
   if (!is.null(df$annData))
+    df$annIntData <- mutate(df$annIntData, intensity = log(intensity, base = base))
+  return(df)
+}
+
+logTransform_ann <- function(df, base = 2) {
+  if (any(df$annIntData$intensity < 0, na.rm = T)) {
+    warning("Negative values detected.\nData are probably already log-transformed or scaled.\nNo transformation applied.")
+    return(df)
+  }
+  if (is.log(df$annIntData$intensity)) {
+    warning("Data are already log-transformed, no transformation applied.")
+    return(df)
+  }
+  
     df$annIntData <- mutate(df$annIntData, intensity = log(intensity, base = base))
   return(df)
 }
