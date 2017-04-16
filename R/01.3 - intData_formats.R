e42c864c58775b2015934c8db9d56e0176af9727
kbajdzienko <krzysztof.bajdzienko@gmail.com>
2017-04-16 17:31:26 +0200

    Add functions to work with phosphorylation site intensity data.

--- R/01.3 - intData_formats.R
+++ R/01.3 - intData_formats.R
@@ -35,3 +35,40 @@ intData <- function(intTable) {
     stop("The intensity table is not a data frame neither a matrix")
   }
 }
+
+
+intTable_ann <- function (df) {
+  intTable_ann <-
+    df$annIntData %>%
+    #mutate(sample_ID = stringr::str_extract(sample_ID, "(\\d{1,}$)")) %>%
+    spread(sample_ID, intensity)
+  return(intTable_ann)
+}
+
+# Function to extract a classic intensity matrix with peaks in rows, samples in columns
+# Output is a matrix with rows called
+intMatrix_ann <- function (df) {
+  intMatrix_ann <- intTable_ann(df)
+  intMatrix_ann <-
+    intMatrix_ann %>%
+    select(-ann_ID) %>%
+    as.matrix() %>%
+    'rownames<-'(intMatrix_ann$ann_ID)
+  return(intMatrix_ann)
+}
+
+# Reverse function to convert product of intTable or intMatrix into tidy format
+intData_ann <- function(intTable_ann) {
+  if (is.data.frame(intTable_ann)) {
+    intTable_ann <-
+      tbl_df(intTable_ann) %>%
+      gather(sample_ID, intensity, -ann_ID)
+  } else if (is.matrix(intTable_ann)) {
+    intTable_ann <-
+      tbl_df(intTable_ann) %>%
+      mutate(ann_ID = rownames(intTable_ann)) %>%
+      gather(sample_ID, intensity, -ann_ID)
+  } else {
+    stop("The intensity table is not a data frame neither a matrix")
+  }
+}
